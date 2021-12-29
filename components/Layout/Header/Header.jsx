4ac4e818ce66090d532c0b5fd489145799b903b1
JohnB <jhondark@gmail.com>
2021-12-29 10:17:39 -0500

    menor update

--- components/Layout/Header/Header.jsx
+++ components/Layout/Header/Header.jsx
@@ -2,9 +2,6 @@
 //styles
 import styles from "./header.module.scss"
 
-//libraries
-import Image from "next/image"
-
 //components
 import SearchBar from "../../SearchBar/SearchBar"
 
@@ -12,17 +9,14 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.header__container}>
-        <Image
+        <img
+          className={styles.header__container_logo}
           src="/assets/images/PRAXU_logo.png"
           alt="logo praxu"
-          width={200}
-          height={28}
         />
-
         <div>
           <SearchBar />
         </div>
-
         <nav className={styles.header__container__menu}>
           <ul>
             <li>
